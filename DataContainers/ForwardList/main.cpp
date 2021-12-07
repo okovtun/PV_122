3800692e325ea7c420a89e6547fecdd47712c614
Clayman <Clayman@DESKTOP-DM8E77U>
2021-12-07 20:26:43 +0200

    ForwardList day 1

---
+++ DataContainers/ForwardList/main.cpp
@@ -0,0 +1,76 @@
+﻿//Solutoin:	DataContainers
+//Project:	ForwardList
+#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+class Element
+{
+	int Data;		//Значение элемента
+	Element* pNext;	//Адрес следующего элемента
+public:
+	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
+	{
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		cout << "EDestructor:\t" << this << endl;
+	}
+	friend class ForwardList;
+};
+
+class ForwardList
+{
+	Element* Head;	//Указывает на начальный элемен списка. 
+	//Является точкой входа в список.
+public:
+	ForwardList()
+	{
+		this->Head = nullptr;	//Если Голова указывает на 0, значит список пуст
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~ForwardList()
+	{
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//						Adding elements:
+	void push_front(int Data)
+	{
+		//1) Создаем элемент:
+		Element* New = new Element(Data);
+		//2) Присоединяем новый элемент к списку:
+		New->pNext = Head;
+		//3) Переносим Голову на Новый элемент:
+		Head = New;
+	}
+
+	//						Methods:
+	void print()const
+	{
+		Element* Temp = Head;	//Temp - это итератор.
+		//Итератор - это указатель, при помощи которого можно получить доступ к элементам структуры данных.
+		while (Temp != nullptr)
+		{
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+			Temp = Temp->pNext;	//Переход на следующий элемент.
+		}
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;	//Размер списка
+	cout << "Введите размер списка: "; cin >> n;
+	ForwardList list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	list.print();
+}

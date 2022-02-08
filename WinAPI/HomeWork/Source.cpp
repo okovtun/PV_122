042cff12ec450d0e0e393d17efc8d8a86ca948b0
Clayman <Clayman@DESKTOP-DM8E77U>
2022-02-08 17:55:33 +0200

    HomeWork

---
+++ WinAPI/HomeWork/Source.cpp
@@ -0,0 +1,54 @@
+﻿#include <Windows.h>
+#include"resource.h"
+//#define MESAGE_BOX
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
+{
+#ifdef MESAGE_BOX
+	MessageBox(NULL, "Hello World", "Info",
+		MB_YESNOCANCEL | MB_ICONINFORMATION | MB_SYSTEMMODAL | MB_HELP | MB_DEFBUTTON2 | MB_RIGHT |
+		MB_SETFOREGROUND);
+#endif // MESAGE_BOX
+
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), 0, DlgProc, 0);
+
+	return 0;
+}
+
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:break;
+	case WM_COMMAND:
+	{
+		switch (LOWORD(wParam))
+		{
+		case IDOK:MessageBox(NULL, L"秋の田の"
+			L"\nかりほの庵の"
+			L"\n苫をあらみ"
+			L"\nわが衣手は"
+			L"\nつゆにぬれつつ\n"
+
+			"\nЧтение :"
+			"\naki no ta no"
+			"\nkario no io no"
+			"\ntoma wo arami"
+			"\nwagakoromode ha"
+			"\ntsuyu ni nuretsutsu\n"
+			"\nПеревод:"
+			"\nО, ночь невзгоды!"
+			"\nКак платье отсырело!"
+			"\nВ сырую осень"
+			"\nВ лачуге бедной плохо под крышею сквозною!", L"\nかりほの庵の", MB_ICONINFORMATION); break;
+		case IDCANCEL: EndDialog(hwnd, 0); break;
+		}
+	}
+	break;
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
